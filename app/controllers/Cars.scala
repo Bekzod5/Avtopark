2236c5d6d88b1058e632e3d463752e8a2e5bbdd1
Bekzod <bekk.tatu@gmail.com>
2015-03-24 12:47:12 +0500

    ozgarishlar boldi

--- app/controllers/Cars.scala
+++ app/controllers/Cars.scala
@@ -20,5 +20,28 @@ class Cars  extends Controller {
     Logger.info(s"SHOW_ALL=${car.list}")
     Ok(views.html.carlist(car.list))
   }
+    def addcars = Action {
+    Ok(views.html.addcars())
+  }
+
+  def add = DBAction { implicit request =>
+    val formParams = request.body.asFormUrlEncoded
+    val nomi = formParams.get("nomi")(0)
+    val modeli = formParams.get("modeli")(0)
+
+
+    println("Nomi: " + nomi)
+    println("Nomi: " + nomi)
+    val carId = (car returning car.map(_.id)) += jadval(None, nomi, modeli)
+    Redirect(routes.Cars.list())
+  }
+
+
+  def remove(id: Int) = DBAction { implicit request =>
+    car.filter(_.id === id).delete;
+    Redirect(routes.Cars.list())
+  }
+
 
 }
+
